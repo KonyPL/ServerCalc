c9f1816c0ff2559d0ecbfba427284b31a52af3d1
KonyPL <44375341+KonyPL@users.noreply.github.com>
2019-10-27 20:57:59 +0100

    Add files via upload
    Added further exception handling and ability to shut server by client and continuing calculations

--- TSprojectClient/client.py
+++ TSprojectClient/client.py
@@ -16,57 +16,163 @@ flags = 0b010
 
 # bin(x)[2:].zfill(y) <- might be helpful ; x - value in decimal to change to binary string; y - total length of the
 # value needed
+pwr = True
+while pwr:
+    status = 0b00
+    # 00 - all good
+    # 01 - overflow error
+    # 10 - dividing by zero
+    # 11 - error (connection and other not specified before)
 
-status = 0b00
-# 00 - all good
-# 01 - overflow error
-# 10 - dividing by zero
-# 11 - error (connection and other not specified before)
+    # >? - comparison - uses the 3rd flag to signalise if values are equal and returns the bigger one
+    while True:
+        operation = input("Choose operation type[+, -, *, /, %, ^, >?, root]: ")
+        op = 0b000
+        if operation == '+':
+            op = 0b000
+            break
+        if operation == '-':
+            op = 0b001
+            break
+        if operation == '*':
+            op = 0b010
+            break
+        if operation == '/':
+            op = 0b011
+            break
+        if operation == '%':
+            op = 0b100
+            break
+        if operation == '^':
+            op = 0b101
+            break
+        if operation == '>?':
+            op = 0b110
+            break
+        if operation == 'root':
+            op = 0b111
+            break
+    while True:
+        try:
+            val1 = int(input("Insert first value (max: 4 294 967 295): "))
+            if val1 > 4294967295:
+                raise OverflowError("Value is too big. ")
+            if val1 < 0:
+                raise OverflowError("Value is negative. This program works only with positive values. ")
+            break
+        except ValueError:
+            print("That is not an integer! Try again:")
+        except OverflowError as error:
+            print(str(error) + "Try again:")
+    while True:
+        try:
+            val2 = int(input("Insert second value (max: 4 294 967 295): "))
+            if val1 > 4294967295:
+                raise OverflowError
+            break
+        except ValueError:
+            print("That is not an integer! Try again:")
+        except OverflowError:
+            print("Value is too big. Try again:")
+    while True:
+        serverpwr = input("Do you want to shut server down? ")
+        if serverpwr == "Yes" or serverpwr == "yes" or serverpwr == "Y" or serverpwr == "y":
+            status = 3
+            break
+        elif serverpwr == "No" or serverpwr == "no" or serverpwr == "N" or serverpwr == "n":
+            break
 
-operation = input("Choose operation type[+, -, *, /, %, ^, >?, root]: ")
+    print("Value 1: " + str(val1))
+    print("Value 2: " + str(val2))
+    print("Operation type: " + str(op))
+    print("Status: " + str(status))
+    print("Session id: " + str(sessionId))
+    print("Flags: " + str(flags))
 
-# >? - comparison - uses the 3rd flag to signalise if values are equal and returns the bigger one
+    binaryPacket = op*(2**77) + val1*(2**45) + val2*(2**13) + status*(2**11) + sessionId*(2**3) + flags
 
-# Change these if you can:
-op = 0b000
-if operation == '+':
-    op = 0b000
-if operation == '-':
-    op = 0b001
-if operation == '*':
-    op = 0b010
-if operation == '/':
-    op = 0b011
-if operation == '%':
-    op = 0b100
-if operation == '^':
-    op = 0b101
-if operation == '>?':
-    op = 0b110
-if operation == 'root':
-    op = 0b111
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(binaryPacket.to_bytes(10, 'big'))
+        data = s.recv(1024)
+        # start of data processing
+        # getting variables
+        data = int.from_bytes(data, 'big')
+        bitShift = ((2 ** 3), (2 ** 11), (2 ** 13), (2 ** 45), (2 ** 77))
+        # getting flags
+        temp = data % bitShift[0]
+        flags = temp
+        data -= temp
+        # getting sessionId
+        temp = data % bitShift[1]
+        sessionId = int(temp / bitShift[0])
+        data -= temp
+        # getting status
+        temp = data % bitShift[2]
+        status = int(temp / bitShift[1])
+        data -= temp
+        # getting result2
+        temp = data % bitShift[3]
+        result2 = int(temp / bitShift[2])
+        data -= temp
+        # getting result1
+        temp = data % bitShift[4]
+        result1 = int(temp / bitShift[3])
+        # getting op
+        data -= temp
+        op = int(data / bitShift[4])
+        # end of getting variables
 
-val1 = int(input("Insert first value (max: 4 294 967 296): "))
-# make this adequate for each operation!
-val2 = int(input("Insert second value (max: 4 294 967 296): "))
+        print("Result 1: " + str(result1))
+        print("Operation type: " + str(op))
+        print("Status: " + str(status))
+        print("Session id: " + str(sessionId))
+        print("Flags: " + str(flags))
 
-print("Value 1: " + str(val1))
-print("Value 2: " + str(val2))
-print("Operation type: " + str(op))
-print("Status: " + str(status))
-print("Session id: " + str(sessionId))
-print("Flags: " + str(flags))
+        if status != 0:
+            if status == 1:
+                print("Error! Result of operation caused overflow!")
+            elif status == 2:
+                print("Error! Division by zero!")
+            else:
+                print("Error! Something went wrong!")
+        else:
+            if flags == 1 or flags == 3 or flags == 5 or flags == 7:
+                flags -= 1
+                print("Both values are equal")
+            else:
+                if op == 0:
+                    operation = ' + '
+                if op == 1:
+                    operation = ' - '
+                if op == 2:
+                    operation = ' * '
+                if op == 3:
+                    operation = ' / '
+                if op == 4:
+                    operation = ' % '
+                if op == 5:
+                    operation = ' ^ '
+                if op == 6:
+                    print("The " + str(result1) + " is greater than other")
+                elif op == 7:
+                    if val2 == 1:
+                        order = 'st'
+                    elif val2 == 2:
+                        order = 'nd'
+                    elif val2 == 3:
+                        order = 'rd'
+                    else:
+                        order = 'th'
 
-binaryPacket = op*(2**77) + val1*(2**45) + val2*(2**13) + status*(2**11) + sessionId*(2**3) + flags
-
-print(binaryPacket)
-print(bin(binaryPacket))
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(binaryPacket.to_bytes(10, 'big'))
-    data = s.recv(1024)
-
-# before repeating querry change last flag (--*) to zero!
-
-print('Received', repr(data))
+                    print(str(val2) + order + " root of " + str(val1) + " = " + str(result1))
+                else:
+                    # showing the result
+                    print(str(val1) + operation + str(val2) + " = " + str(result1))
+    while True:
+        calc = input("Do you want to make another calculation? ")
+        if calc == "Yes" or calc == "yes" or calc == "Y" or calc == "y":
+            break
+        elif calc == "No" or calc == "no" or calc == "N" or calc == "n":
+            pwr = False
+            break
--- TSprojectClient/server.py
+++ TSprojectClient/server.py
@@ -5,88 +5,108 @@ import math
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            # start of data processing
-            # getting variables
-            data = int.from_bytes(data, 'big')
-            bitShift = ((2**3), (2**11), (2**13), (2**45), (2**77))
-            # getting flags
-            temp = data % bitShift[0]
-            flags = temp
-            data -= temp
-            # getting sessionId
-            temp = data % bitShift[1]
-            sessionId = int(temp / bitShift[0])
-            data -= temp
-            # getting status
-            temp = data % bitShift[2]
-            status = int(temp / bitShift[1])
-            data -= temp
-            # getting val2
-            temp = data % bitShift[3]
-            val2 = int(temp / bitShift[2])
-            data -= temp
-            # getting val1
-            temp = data % bitShift[4]
-            val1 = int(temp / bitShift[3])
-            # getting op
-            data -= temp
-            op = int(data / bitShift[4])
-            # end of getting variables
+pwr = True
+while pwr:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                # start of data processing
+                # getting variables
+                data = int.from_bytes(data, 'big')
+                bitShift = ((2**3), (2**11), (2**13), (2**45), (2**77))
+                # getting flags
+                temp = data % bitShift[0]
+                flags = temp
+                data -= temp
+                # getting sessionId
+                temp = data % bitShift[1]
+                sessionId = int(temp / bitShift[0])
+                data -= temp
+                # getting status
+                temp = data % bitShift[2]
+                status = int(temp / bitShift[1])
+                data -= temp
+                # getting val2
+                temp = data % bitShift[3]
+                val2 = int(temp / bitShift[2])
+                data -= temp
+                # getting val1
+                temp = data % bitShift[4]
+                val1 = int(temp / bitShift[3])
+                # getting op
+                data -= temp
+                op = int(data / bitShift[4])
+                data = 0
+                # end of getting variables
+                if status == 3:
+                    pwr = False
+                    status = 0
+                # sessionId setting
+                if flags == 7 or flags == 6 or flags == 3 or flags == 2:
+                    sessionId = random.randint(1, 255)
+                    flags -= 2
 
-            # sessionId setting
-            if flags == 7 or 6 or 3 or 2:
-                sessionId = random.randint(1, 255)
-                flags -= 2
+                # math operations
+                try:
+                    if op == 0:
+                        if val1 + val2 > 4294967295:
+                            val1 = val2 = 0
+                            raise OverflowError
+                        val1 = val1 + val2
+                    if op == 1:
+                        if val2 > val1:
+                            val1 = val2 = 0
+                            raise OverflowError
+                        val1 = val1 - val2
+                    if op == 2:
+                        if val1 * val2 > 4294967295:
+                            val1 = val2 = 0
+                            raise OverflowError
+                        val1 = val1 * val2
+                    if op == 3:
+                        val1 = int(val1 / val2)
+                    if op == 4:
+                        val1 = val1 % val2
+                    if op == 5:
+                        if val1 ** val2 > 4294967295:
+                            val1 = val2 = 0
+                            raise OverflowError
+                        val1 = val1**val2
+                    if op == 6:
+                        if val1 == val2:
+                            flags += 1
+                        elif val2 > val1:
+                            val1 = val2
+                    if op == 7:
+                        val1 = val1 ** (1. / val2)
+                        val1 = int(val1)
 
-            # math operations
-            try:
-                if op == 0:
-                    val1 = val1 + val2
-                if op == 1:
-                    val1 = val1 - val2
-                if op == 2:
-                    val1 = val1 * val2
-                if op == 3:
-                    val1 = val1 / val2
-                if op == 4:
-                    val1 = val1 % val2
-                if op == 5:
-                    val1 = val1**val2
-                if op == 6:
-                    if val1 == val2:
-                        flags += 1
-                    elif val2 > val1:
-                        val1 = val2
-                if op == 7:
-                    val1 = val1 ** (1. / val2)
-                    val1 = int(val1)
+                    val2 = 0
+                    status = 0
 
-                val2 = 0
-                status = 0
-
-            except ZeroDivisionError:
-                status = 2
-            except OverflowError:
-                status = 1
+                except ZeroDivisionError:
+                    status = 2
+                except OverflowError:
+                    status = 1
 
 
-            print("Value 1: " + str(val1))
-            print("Value 2: " + str(val2))
-            print("Operation type: " + str(op))
-            print("Status: " + str(status))
-            print("Session id: " + str(sessionId))
-            print("Flags: " + str(flags))
+                print("Value 1: " + str(val1))
+                print("Value 2: " + str(val2))
+                print("Operation type: " + str(op))
+                print("Status: " + str(status))
+                print("Session id: " + str(sessionId))
+                print("Flags: " + str(flags))
 
-            data = op * (2**77) + val1 * (2**45) + val2 * (2**13) + status * (2**11) + sessionId * (2**3) + flags
-            # end of processing data
-            conn.sendall(data.to_bytes(10, 'big'))
+                data = op * (2**77) + val1 * (2**45) + val2 * (2**13) + status * (2**11) + sessionId * (2**3) + flags
+                # end of processing data
+                conn.sendall(data.to_bytes(10, 'big'))
+
+
+
